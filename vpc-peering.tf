ef14efe39cee5c20c30c1b4dee06236c22865879
José Roberto <31750084+git-jrprado@users.noreply.github.com>
2021-01-24 14:31:21 -0300

    Update vpc-peering.tf
    Fixed allow_remote_vpc_dns_resolution

--- vpc-peering.tf
+++ vpc-peering.tf
@@ -4,18 +4,28 @@ resource "aws_vpc_peering_connection" "requester" {
   peer_owner_id = var.accepter_owner_id
   vpc_id        = var.requester_vpc_id
   peer_region   = var.accepter_region
-
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
 }
 
 resource "aws_vpc_peering_connection_accepter" "accepter" {
   provider                  = aws.accepter
   vpc_peering_connection_id = aws_vpc_peering_connection.requester.id
   auto_accept               = true
-  
+}
+
+resource "aws_vpc_peering_connection_options" "requester" {
+  provider                  = aws.requester
+  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.accepter.id
+
+  requester {
+    allow_remote_vpc_dns_resolution  = true
+  }
+}
+
+resource "aws_vpc_peering_connection_options" "accepter" {
+  provider                  = aws.accepter
+  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.accepter.id
+
   accepter {
-    allow_remote_vpc_dns_resolution = true
+    allow_remote_vpc_dns_resolution  = true
   }
 }
